b8ddd9da182b2d1951f5e1552625c254c16374ac
eri24816 <a931eric@gmail.com>
2023-02-01 15:56:39 +0000

    basic code

---
+++ bot.py
@@ -0,0 +1,51 @@
+import asyncio
+import discord
+import json
+import os
+from discord.ext import commands
+
+
+def create_require_json():
+    
+    """filenames = []
+    data = {}
+    for filename in filenames:
+        if os.path.exists(filename):
+            pass
+        else:
+            with open(filename, "w") as jfile:
+                json.dump(data, jfile)"""
+
+    if os.path.exists("not_token.json"):
+        pass
+    else:
+        TOKEN = input("paste your bot's token here: ")
+        data = {
+            "TOKEN": TOKEN
+        }
+        with open("not_token.json", "w") as jfile:
+            json.dump(data, jfile)
+    print("require jsons are created.")
+
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.all()
+        owners = [1022080471506624545, 364976571192311808]
+        super().__init__(command_prefix="t", intents=intents, owner_ids=set(owners))
+
+    async def on_ready(self):
+        print("Online.")
+
+    async def setup_hook(self):
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                await bot.load_extension(f"cogs.{filename[:-3]}")
+
+
+bot = MyBot()
+if __name__ == "__main__":
+    create_require_json()
+    with open("not_token.json", mode="r", encoding="utf8") as jfile:
+        jdata = json.load(jfile)
+    bot.run(jdata["TOKEN"])
---
+++ cogs/activity.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extension
+import asyncio
+
+
+class Activity(Cog_Extension):
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Activity cog loaded.")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        async def activiy_task():
+            await self.bot.wait_until_ready()
+            while not self.bot.is_closed():
+                activitys = ["運作平台: Eri24816 租的 server",
+                             "Made By GreenSlime1024", "還沒做好"]
+                for i in activitys:
+                    await self.bot.change_presence(activity=discord.Game(i))
+                    await asyncio.sleep(15)
+
+        self.bg_task = self.bot.loop.create_task(activiy_task())
+
+
+async def setup(bot):
+    await bot.add_cog(Activity(bot))
